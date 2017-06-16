84607511e19aaeb55dcaa1576be6d203822d0845
Katelyn Kasperowicz <kat3kasper@gmail.com>
2017-06-16 15:42:37 -0400

    Add scripts for server and client can run concurrently

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const request = require('request')
 require('dotenv').config()
 const app = express()
 
+app.set('port', (process.env.PORT || 3001));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
@@ -20,6 +22,6 @@ app.get('/country/:country', function(req, res) {
   })
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(app.get('port'), function () {
+  console.log(`Example app listening on port ${app.get('port')}!`)
 })
