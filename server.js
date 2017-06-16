07ed888f667ca692a966bc94e29a0bc9e620e569
Katelyn Kasperowicz <kat3kasper@gmail.com>
2017-06-16 15:43:49 -0400

    Reformat last.fm response to only include necessary info

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.get('/country/:country', function(req, res) {
+function formatTracks(body) {
+  let tracks = [];
+  let jsonBody = JSON.parse(body)
+  jsonBody.tracks.track.forEach((track, index) => {
+    tracks.push({name: track.name, listeners: track.listeners, artist: track.artist.name, rank: parseInt(track["@attr"].rank)+1, image: track.image[1]["#text"]});
+  })
+  return {tracks};
+}
+
+app.get('/api/country/:country', function(req, res) {
   request(`http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${req.params.country}&api_key=${process.env.LAST_FM_API_KEY}&format=json&limit=5`, (error, response, body) => {
     if (error) {
       const status = response && response.statusCode ? response.statusCode : 500;
@@ -17,7 +26,7 @@ app.get('/country/:country', function(req, res) {
     } else if (JSON.parse(body).error) {
       res.status(500).json({error: JSON.parse(body).message});
     } else {
-      res.status(200).json(JSON.parse(body));
+      res.status(200).json(formatTracks(body));
     }
   })
 })
