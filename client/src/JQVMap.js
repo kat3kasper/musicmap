fa5d5eb63d1692b83f98c302f587c28cec7f2254
Katelyn Kasperowicz <kat3kasper@gmail.com>
2017-06-29 18:38:08 -0600

    Render map into seperate div rather than component

--- client/src/JQVMap.js
+++ client/src/JQVMap.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import './JQVMap.css';
 import './jquery.vmap.js';
 import './jquery.vmap.world.js';
@@ -8,16 +7,33 @@ import $ from 'jquery';
 class JQVMap extends Component {
 
   componentDidMount() {
-    $(ReactDOM.findDOMNode(this)).vectorMap({
+    $('#vmap').vectorMap({
       backgroundColor: '#ffffff',
       enableZoom: true,
-      onRegionClick: this.props.countryClickedFunction
+      onRegionClick: this.props.countryClickedFunction,
+      onLabelShow: this.onLabelShow
     });
+  }
+
+  onLabelShow(event, label, code) {
+    event.preventDefault();
+  }
+
+  zoomIn() {
+    $('#vmap').vectorMap('zoomIn');
+  }
+
+  zoomOut() {
+    $('#vmap').vectorMap('zoomOut');
   }
 
   render() {
     return (
-      <div className="JQVMap"></div>
+      <div className="JQVMap">
+        <div id="vmap"></div>
+        <button onClick={this.zoomOut}>Zoom Out</button>
+        <button onClick={this.zoomIn}>Zoom In</button>
+      </div>
     );
   }
 }
